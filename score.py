d0376e1d62a34528ae45ac81dffe7a105b9107f0
Matt Hielscher <mhielscher@gmail.com>
2013-03-06 14:57:13 -0800

    Moved weapon data to separate file, separated print function.

--- score.py
+++ score.py
@@ -5,139 +5,21 @@ import random
 import signal
 import simplejson
 
-# (((range, attack, damage), ...), target_score)
+from weapons import *
 
-weapons = {
-    "16-pound Cannon":
-        {"range": [2,5,8,11,14],
-         "attack": [4,2,0,-2,-4],
-         "damage": {(1,2): [2,1,1,1,1],
-                    (3,4): [3,2,2,2,1],
-                    (5,6): [4,3,2,2,1],
-                    (7,8): [5,4,2,2,1],
-                    (9,0): [6,5,3,3,1]
-                   },
-         #"points": 2
-         "points": 1.838
-        },
-    "16-pound Carronade":
-        {"range": [1,2,4,5,7],
-         "attack": [4,2,0,-2,-4],
-         "damage": {(1,2): [3,2,1,1,1],
-                    (3,4): [5,3,2,2,1],
-                    (5,6): [6,4,3,2,1],
-                    (7,8): [7,5,4,2,1],
-                    (9,0): [9,6,5,3,1]
-                   },
-         #"points": 1
-         "points": 1.129
-        },
-    "16-pound Culverin":
-        {"range": [4,8,12,16,21],
-         "attack": [4,2,0,-2,-4],
-         "damage": {(1,2): [1,1,1,1,1],
-                    (3,4): [2,2,2,1,1],
-                    (5,6): [3,2,2,1,1],
-                    (7,8): [4,2,2,1,1],
-                    (9,0): [5,3,3,1,1]
-                   },
-         #"points": 2
-         "points": 2.231
-        },
-    "16-pound Mortar":
-        {"range": [2,5,8,11,14],
-         "attack": [2,1,-1,-3,-4],
-         "damage": {(1,2): [2,2,1,1,1],
-                    (3,4): [4,3,2,2,1],
-                    (5,6): [5,4,3,2,1],
-                    (7,8): [6,5,4,2,1],
-                    (9,0): [8,6,5,3,1]
-                   },
-         #"points": 2
-         "points": 1.876
-        },
-    "16-pound Howitzer":
-        {"range": [2,5,8,11,14],
-         "attack": [6,3,1,-1,-4],
-         "damage": {(1,2): [1,1,1,1,1],
-                    (3,4): [2,2,2,1,1],
-                    (5,6): [3,2,2,1,1],
-                    (7,8): [4,2,2,1,1],
-                    (9,0): [5,3,3,1,1]
-                   },
-         #"points": 1.5
-         "points": 1.643
-        },
-    "32-pound Cannon":
-        {"range": [4,8,13,17,22],
-         "attack": [4,2,0,-2,-4],
-         "damage": {(1,2): [3,2,1,1,1],
-                    (3,4): [5,3,2,2,1],
-                    (5,6): [6,4,3,2,1],
-                    (7,8): [7,5,4,2,1],
-                    (9,0): [9,6,5,3,1]
-                   },
-         #"points": 4
-         "points": 4.14
-        },
-    "32-pound Carronade":
-        {"range": [2,4,6,8,11],
-         "attack": [4,2,0,-2,-4],
-         "damage": {(1,2): [4,3,2,1,1],
-                    (3,4): [7,5,3,2,2],
-                    (5,6): [9,6,4,3,2],
-                    (7,8): [11,7,5,4,2],
-                    (9,0): [14,9,6,5,3]
-                   },
-         #"points": 3
-         "points": 2.967
-        },
-    "32-pound Culverin":
-        {"range": [6,13,19,26,33],
-         "attack": [4,2,0,-2,-4],
-         "damage": {(1,2): [2,2,1,1,1],
-                    (3,4): [4,3,2,2,1],
-                    (5,6): [5,4,3,2,1],
-                    (7,8): [6,5,4,2,1],
-                    (9,0): [8,6,5,3,1]
-                   },
-         #"points": 6
-         "points": 6.044
-        },
-    "32-pound Mortar":
-        {"range": [4,8,13,17,22],
-         "attack": [2,1,-1,-3,-4],
-         "damage": {(1,2): [4,2,2,1,1],
-                    (3,4): [6,4,3,2,2],
-                    (5,6): [8,5,4,2,2],
-                    (7,8): [10,6,5,2,2],
-                    (9,0): [12,8,6,3,3]
-                   },
-         #"points": 4.5
-         "points": 4.25
-        },
-    "32-pound Howitzer":
-        {"range": [4,8,13,17,22],
-         "attack": [6,3,1,-1,-4],
-         "damage": {(1,2): [2,2,1,1,1],
-                    (3,4): [4,3,2,2,1],
-                    (5,6): [5,4,3,2,1],
-                    (7,8): [6,5,4,2,1],
-                    (9,0): [8,6,5,3,1]
-                   },
-         #"points": 4.5
-         "points": 4.732
-        }
-}
-
-#transpose
-weapons_columns = {name: zip(d['range'], d['attack'], *(dmg[1] for dmg in sorted(d['damage'].items()))) for name, d in weapons.items()}
-
-#flatten
-weapons_data = [(name, [n for c in weapons_columns[name] for n in c], weapons[name]["points"]) for name in weapons]
-data_len = len(weapons_data[0][1])
+def print_points(error, coef, fitness):
+    print "Coefficients: %s" % (coef)
+    total_diff = 0
+    for name, data, target in weapons_data:
+        fitn = fitness(data, coef, target)
+        diff = target - fitn
+        total_diff += diff
+        print "%s: %f calc pts vs. %f target pts (%f delta)" % (name, fitn, target, diff)
+    print "Max delta from target: %f" % (error)
+    print "Mean difference: %f" % (total_diff/len(weapons_data))
 
 class Linear:
+    
     def score(self, data, coefficients):
         equation = zip(data, coefficients)
         total = 0
@@ -154,7 +36,8 @@ class Linear:
     def fitness(self, coefficients):
         if len(coefficients) != data_len:
             print "Coefficients of wrong length: %d vs. %d" % (len(coefficients), data_len)
-            return sys.maxint
+            print "Padding coefficients with 0's."
+            coefficients += [0]*(len(data)-len(coefficients))
         
         total_fitness = 0
         for name, data, target in weapons_data:
@@ -175,10 +58,9 @@ class Linear:
         
         def sig_handler(signal, frame):
             print "Search interrupted."
-            print "Best error: %f" % (best[0])
-            print "Coefficients: %s" % (best[1])
+            print_points(best[0], best[1], self.individual_fitness)
             sys.exit(0)
-        signal.signal(signal.SIGINT, self.sig_handler)
+        signal.signal(signal.SIGINT, sig_handler)
         
         steps = 0
         prev_best_err = sys.maxint
@@ -197,7 +79,7 @@ class Linear:
                         d_coef[i] *= 1.1
                     else:
                         coef[i] -= 2*d_coef[i]
-                        err = fitness(coef)
+                        err = self.fitness(coef)
                         if err < best[0]:
                             best = (err, coef)
                             d_coef[i] *= 1.1
@@ -205,7 +87,8 @@ class Linear:
                             coef[i] += d_coef[i]
                             d_coef[i] *= 0.9
                 steps += 1
-                print "Twiddle #%d -> %f %f %f" % (steps, best[0], err, sum(d_coef))
+                if steps % 1000 == 0:
+                    print "Twiddle #%d -> %f %f %f" % (steps, best[0], err, sum(d_coef))
         return coef
         
     def random_search(self, coef=None, minimum=1):
@@ -219,10 +102,9 @@ class Linear:
         
         def sig_handler(signal, frame):
             print "Search interrupted."
-            print "Best error: %f" % (best[0])
-            print "Coefficients: %s" % (best[1])
+            print_points(best[0], best[1], self.individual_fitness)
             sys.exit(0)
-        signal.signal(signal.SIGINT, self.sig_handler)
+        signal.signal(signal.SIGINT, sig_handler)
         
         steps = 0
         while err > minimum:
@@ -231,7 +113,8 @@ class Linear:
             if err < best[0]:
                 best = (err, coef)
             steps += 1
-            print "Random search #%d -> %f %f" % (steps, best[0], err)
+            if steps % 1000 == 0:
+                print "Random search #%d -> %f %f" % (steps, best[0], err)
         return coef
 
 
@@ -281,9 +164,9 @@ class Nonlinear:
         
         def sig_handler(signal, frame):
             print "Search interrupted."
-            print "Best error: %f" % (best[0])
-            print "Coefficients: %s" % (best[1])
             print "Exponents: %s" % (best[2])
+            finish(best[1], self.individual_fitness)
+            print_points(best[0], best[1], self.individual_fitness)
             sys.exit(0)
         signal.signal(signal.SIGINT, sig_handler)
         
@@ -321,14 +204,15 @@ class Nonlinear:
                             d_coef[i] *= 0.9
                             d_exp[i] *= 0.9
                 steps += 1
-                print "Twiddle #%d -> %f, %f, %f, %f" % (steps, best[0], err, sum(d_coef), sum(d_exp))
+                if steps % 1000 == 0:
+                    print "Twiddle #%d -> %f, %f, %f, %f" % (steps, best[0], err, sum(d_coef), sum(d_exp))
         return coef, exp
         
     def random_search(self, coefficients=None, exponents=None, minimum=0.75):
         if not coefficients:
-                coef = [random.uniform(-1,1) for i in range(data_len)]
-            if not exponents:
-                exp = [random.uniform(-1,1) for i in range(data_len)]
+            coef = [random.uniform(-1,1) for i in range(data_len)]
+        if not exponents:
+            exp = [random.uniform(-1,1) for i in range(data_len)]
             d_coef = [random.uniform(0,2) for i in range(data_len)]
             d_exp = [random.uniform(0,2) for i in range(data_len)]
         err = fitness(coef, exp)
@@ -336,9 +220,8 @@ class Nonlinear:
         
         def sig_handler(signal, frame):
             print "Search interrupted."
-            print "Best error: %f" % (best[0])
-            print "Coefficients: %s" % (best[1])
             print "Exponents: %s" % (best[2])
+            print_points(best[0], best[1], self.individual_fitness)
             sys.exit(0)
         signal.signal(signal.SIGINT, sig_handler)
         
@@ -350,7 +233,8 @@ class Nonlinear:
             if err < best[0]:
                 best = (err, coef, exp)
             steps += 1
-            print "Random search #%d -> %f, %f, %f" % (steps, best[0], err)
+            if steps % 1000 == 0:
+                print "Random search #%d -> %f, %f, %f" % (steps, best[0], err)
         return coef, exp
 
 if __name__ == "__main__":
@@ -359,12 +243,18 @@ if __name__ == "__main__":
     rnd_str = f.read(8)
     random.seed(rnd_str)
     
+    model = Linear()
+    #model = Nonlinear()
+    
     if len(sys.argv) > 1:
         coefficients = simplejson.loads(sys.argv[1])
     else:
         coefficients = None
-        coefficients = random_search()
-    print twiddle(coefficients)
+        coefficients = model.random_search()
+    #print model.twiddle(coefficients)
+    #print model.random_search(coefficients)
+    print "----"
+    print_points(model.fitness(coefficients), coefficients, model.individual_fitness)
 
 
 
@@ -390,5 +280,10 @@ Coefficients: [0.5579355009914552, -0.24299999777323053, -0.5469939825303045, 0.
 Twiddle linear, latest:
 Best error: 0.592519
 Coefficients: [0.5579003396685076, -0.24259983320387815, -0.5468966372366523, 0.9729635853151729, -0.6719999738867345, 0.6589999992550457, -0.6270000153886274, 0.6512999996509868, -0.15694624295101084, 0.7248897776326051, -0.6211665338266634, 0.8800181438535677, 0.6822000130754932, 0.12200008671992624, -0.9417000000000071, 0.8557892141597758, -0.5772907396009691, -0.14230000217475497, 0.9189999169366563, 0.02995869459919074, -0.6176999774135185, 0.1940000000000003, 0.617475954825495, -0.09046759388914836, 0.18816482114479421, -0.7930461563733091, -0.8631004002268415, -0.0725922638530214, 0.18499999999999828, -0.22179995773509673, -0.04755159140410673, 0.8442625472474002, 0.15830921156878192, 0.41986692949605736, 0.14864667540791923]
+
+
+Random linear, best max deviation for a single point value:
+Best error: 0.238317
+Coefficients: [0.3562316646829533, -0.19254440618896096, -0.898988792922286, -0.179468774651788, 0.286855078156417, 0.9324925204930115, -0.39991235066191955, 0.7453719878808887, 0.9512402475847488, 0.5223158093269609, 0.2544888422424818, -0.4803864745716231, 0.45182327633193875, 0.888010487582295, 0.13771862991256367, 0.12920427599026962, 0.26646805387472017, -0.11998776883866613, -0.3157863394097349, 0.4145587314232686, -0.5396404607862597, -0.20699858135851024, 0.12706847173552838, -0.8760270908850449, 0.13209053403889404, 0.5333395909631973, -0.9810388783254522, -0.4141351598117802, -0.1158573572206234, 0.7329550054423919, 0.76069330197601, -0.6265146181127192, -0.6278369957658332, -0.5855419394516743, 0.12348197297821661]
 
 """
